d10956f055a8bc13ca38faccab3d0ed3290a07bc
Ankit Dutta <Ankit@LAPTOP-LF8HOQMD.(none)>
2021-06-02 01:26:39 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
+
+const app=express();
+dotenv.config();
+app.use(cors());
+
+app.use(bodyParser.json({limit:"30mb",extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
+
+app.get('/',(req,res)=>
+{
+    res.send("Hello to memories API");
+})
+app.use('/posts',postRoutes);
+
+//const CONNECTION_URL="mongodb+srv://admin-ankit:test1234@cluster0.n7kuh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const PORT= process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+.then(()=>app.listen(PORT,()=> console.log(`Server is running on port : ${PORT}`)))
+.catch((error)=>console.log(error.message));
+
+mongoose.set('useFindAndModify',false);
